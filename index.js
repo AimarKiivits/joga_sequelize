2450fbda2cfed87114bbc5d9e9814091fda31d14
Aimar Kiivits <Aimarkiivits1@gmail.com>
2024-06-18 18:58:23 +0300

    Getted all articles data

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ sequelize
         console.error('Unable to connect to the database:', err);
     });
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to the application.' });
-})
+const articleRouter = require('./routes/article');
+
+app.use('/', articleRouter);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
